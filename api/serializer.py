2e6b640c9471318de5dedd4d519154fcbcb31bb9
saqib-devops <saqibahmad778866@gmail.com>
2022-06-11 23:19:59 +0500

    Advance api Creted

--- api/serializer.py
+++ api/serializer.py
@@ -2,42 +2,44 @@ from decimal import Decimal
 
 from rest_framework import serializers
 
-from api.models import Product
+from api.models import Product, Category
 
 
-class CategorySerializer(serializers.Serializer):
-    id = serializers.IntegerField()
+#
+# class ProductSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length=100)
+#     # category = serializers.StringRelatedField()
+#     category = CategorySerializers()
+#     unit_price = serializers.DecimalField(max_digits=8, decimal_places=2, source='price')
+#     tax_price = serializers.SerializerMethodField(method_name='calculated_tax')
+#
+#     def calculated_tax(self, product: Product):
+#         return product.price * Decimal(1.5)
+#     #
+#     # def create(self, validated_data):
+#     #     return Product.objects.create(**validated_data)
+#     #
+#     # def update(self, instance, validated_data):
+#     #     instance.title = validated_data.get('title', instance.title)
+#     #     instance.category = validated_data.get('category', instance.category)
+#     #     instance.price = validated_data.get('price', instance.price)
+#     #
+#     #     instance.save()
+#     #     return instance
+
+
+class CategorySerializers(serializers.Serializer):
     title = serializers.CharField(max_length=200)
 
 
-class ProductSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=100)
-    # category = serializers.StringRelatedField()
-    category = CategorySerializer()
-    unit_price = serializers.DecimalField(max_digits=8, decimal_places=2, source='price')
-    tax_price = serializers.SerializerMethodField(method_name='calculated_tax')
-
-    def calculated_tax(self, product: Product):
-        return product.price * Decimal(1.5)
-    #
-    # def create(self, validated_data):
-    #     return Product.objects.create(**validated_data)
-    #
-    # def update(self, instance, validated_data):
-    #     instance.title = validated_data.get('title', instance.title)
-    #     instance.category = validated_data.get('category', instance.category)
-    #     instance.price = validated_data.get('price', instance.price)
-    #
-    #     instance.save()
-    #     return instance
-
-
 class ProductsSerializers(serializers.ModelSerializer):
-    class Meta:
-        fields = ['title','unit_price', 'tax_price','category']
-
     tax_price = serializers.SerializerMethodField(method_name='calculated_tax')
+    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2, source='price')
+
+    class Meta:
+        model = Product
+        fields = ['title', 'unit_price', 'tax_price','inventory','category']
 
     def calculated_tax(self, product: Product):
         return product.price * Decimal(1.5)
-    category = CategorySerializer()
--- api/views.py
+++ api/views.py
@@ -4,130 +4,251 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.generics import GenericAPIView
+from rest_framework.generics import GenericAPIView, ListAPIView, DestroyAPIView, RetrieveAPIView, CreateAPIView, \
+    UpdateAPIView
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, CreateModelMixin, DestroyModelMixin, \
     UpdateModelMixin
 from rest_framework.views import APIView
 
 from api.models import Product, Category
-from api.serializer import ProductSerializer, CategorySerializer
+from api.serializer import  CategorySerializers, ProductsSerializers
 
 
-class ProductApi(APIView):
-    def get(self, request, pk=None):
-        id = pk
-        if id is not None:
-            product = Product.objects.get(id=pk)
-            product = ProductSerializer(product)
-            return Response(product.data)
-        students = Product.objects.all()
-        serializer = ProductSerializer(students, many=True)
-        return Response(serializer.data)
+# class ProductApi(APIView):
+#     def get(self, request, pk=None):
+#         id = pk
+#         if id is not None:
+#             product = Product.objects.get(id=pk)
+#             product = ProductSerializer(product)
+#             return Response(product.data)
+#         students = Product.objects.all()
+#         serializer = ProductSerializer(students, many=True)
+#         return Response(serializer.data)
+#
+#     def post(self, request, format=None):
+#         serializer = ProductSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def put(self, request, pk=None, format=None):
+#         product = Product.objects.get(id=pk)
+#         serializer = ProductSerializer(product, request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response({'msg': 'Succesfully Update'}, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def patch(self, request, pk=None, format=None):
+#         product = Product.objects.get(id=pk)
+#         serializer = ProductSerializer(product, request.data, partial=True)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response({'msg': 'Succesfully Update'}, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def delete(self, request, pk=None):
+#         product = Product.objects.get(id=pk)
+#         product.delete()
+#         return Response({'Msg': 'Successfully Deleted'}, status=status.HTTP_400_BAD_REQUEST)
+#
+#
+# class ProductList(GenericAPIView, ListModelMixin):
+#     queryset = Product.objects.select_related('category').all()
+#     serializer_class = ProductSerializer
+#
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+#
+#
+# class ProductDetail(GenericAPIView, RetrieveModelMixin):
+#     queryset = Product.objects.select_related('category').all()
+#     serializer_class = ProductSerializer
+#
+#     def get(self, request, *args, **kwargs):
+#         return self.retrieve(request, *args, **kwargs)
+#
+#
+# class ProductCreate(GenericAPIView, CreateModelMixin):
+#     queryset = Product.objects.select_related('category').all()
+#     serializer_class = ProductSerializer
+#
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
+#
+#
+# class ProductUpdate(GenericAPIView, UpdateModelMixin):
+#     queryset = Product.objects.select_related('category').all()
+#     serializer_class = ProductSerializer
+#
+#     def patch(self, request, *args, **kwargs):
+#         return self.update(request, *args, **kwargs)
+#
+#
+# class ProductDelete(GenericAPIView, DestroyModelMixin):
+#     queryset = Product.objects.select_related('category').all()
+#     serializer_class = ProductSerializer
+#
+#     def delete(self, request, *args, **kwargs):
+#         return self.destroy(request, *args, **kwargs)
+#
+#
+# class CategoryList(GenericAPIView, ListModelMixin):
+#     queryset = Category.objects.all()
+#     serializer_class = CategorySerializers
+#
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+#
+#
+# class CategoryDetail(GenericAPIView, RetrieveModelMixin):
+#     queryset = Category.objects.all()
+#     serializer_class = CategorySerializers
+#
+#     def get(self, request, *args, **kwargs):
+#         return self.retrieve(request, *args, **kwargs)
+#
+#
+# class CategoryCreate(GenericAPIView, CreateModelMixin):
+#     queryset = Category.objects.all()
+#     serializer_class = CategorySerializers
+#
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
+#
+#
+# class CategoryUpdate(GenericAPIView, UpdateModelMixin):
+#     queryset = Category.objects.all()
+#     serializer_class = CategorySerializers
+#
+#     def patch(self, request, *args, **kwargs):
+#         return self.update(request, *args, **kwargs)
+#
+#
+# class CategoryDelete(GenericAPIView, DestroyModelMixin):
+#     queryset = Category.objects.all()
+#     serializer_class = CategorySerializers
+#
+#     def delete(self, request, *args, **kwargs):
+#         return self.destroy(request, *args, **kwargs)
 
-    def post(self, request, format=None):
-        serializer = ProductSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def put(self, request, pk=None, format=None):
-        product = Product.objects.get(id=pk)
-        serializer = ProductSerializer(product, request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({'msg': 'Succesfully Update'}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# Generic View
 
-    def patch(self, request, pk=None, format=None):
-        product = Product.objects.get(id=pk)
-        serializer = ProductSerializer(product, request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({'msg': 'Succesfully Update'}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# Products
 
-    def delete(self, request, pk=None):
-        product = Product.objects.get(id=pk)
-        product.delete()
-        return Response({'Msg': 'Successfully Deleted'}, status=status.HTTP_400_BAD_REQUEST)
+class ProductListAPi(ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductsSerializers
 
 
-class ProductList(GenericAPIView, ListModelMixin):
-    queryset = Product.objects.select_related('category').all()
-    serializer_class = ProductSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
+class ProductDetailAPi(RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductsSerializers
 
 
-class ProductDetail(GenericAPIView, RetrieveModelMixin):
-    queryset = Product.objects.select_related('category').all()
-    serializer_class = ProductSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
+class ProductUpdateAPi(UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductsSerializers
 
 
-class ProductCreate(GenericAPIView, CreateModelMixin):
-    queryset = Product.objects.select_related('category').all()
-    serializer_class = ProductSerializer
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+class ProductCreateApi(CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductsSerializers
 
 
-class ProductUpdate(GenericAPIView, UpdateModelMixin):
-    queryset = Product.objects.select_related('category').all()
-    serializer_class = ProductSerializer
-
-    def patch(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
+class ProductDeleteApi(DestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductsSerializers
 
 
-class ProductDelete(GenericAPIView, DestroyModelMixin):
-    queryset = Product.objects.select_related('category').all()
-    serializer_class = ProductSerializer
+# Category
 
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-
-
-class CategoryList(GenericAPIView, ListModelMixin):
+class CategoryListApi(ListAPIView):
     queryset = Category.objects.all()
-    serializer_class = CategorySerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
+    serializer_class = CategorySerializers
 
 
-class CategoryDetail(GenericAPIView, RetrieveModelMixin):
+class CategoryDetailApi(RetrieveAPIView):
     queryset = Category.objects.all()
-    serializer_class = CategorySerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
+    serializer_class = CategorySerializers
 
 
-class CategoryCreate(GenericAPIView, CreateModelMixin):
+class CategoryCreateAPi(CreateAPIView):
     queryset = Category.objects.all()
-    serializer_class = CategorySerializer
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+    serializer_class = CategorySerializers
 
 
-class CategoryUpdate(GenericAPIView, UpdateModelMixin):
+class CategoryUpdateApi(UpdateAPIView):
     queryset = Category.objects.all()
-    serializer_class = CategorySerializer
-
-    def patch(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
+    serializer_class = CategorySerializers
 
 
-class CategoryDelete(GenericAPIView, DestroyModelMixin):
+class CategoryDeleteApi(DestroyAPIView):
     queryset = Category.objects.all()
-    serializer_class = CategorySerializer
+    serializer_class = CategorySerializers
 
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
+
+# Generic View
+
+# If we Want to Customize our detail in Generic View ........
+
+# class ProductLists(ListAPIView):
+#
+#     def get_queryset(self):
+#         return Product.objects.select_related('category').all()
+#
+#     def get_serializer_class(self):
+#         return ProductsSerializers
+#
+#     def get_serializer_context(self):
+#         return self.request
+#
+#
+# class ProductDetails(RetrieveAPIView):
+#
+#     def get_queryset(self):
+#         return Product.objects.select_related('category').all()
+#
+#     def get_serializer_class(self):
+#         return ProductsSerializers
+#
+#     def get_serializer_context(self):
+#         return self.request
+#
+#
+# class ProductUpdates(UpdateAPIView):
+#
+#     def get_queryset(self):
+#         return Product.objects.select_related('category').all()
+#
+#     def get_serializer_class(self):
+#         return ProductsSerializers
+#
+#     def get_serializer_context(self):
+#         return self.request
+#
+#
+# class ProductCreates(CreateAPIView):
+#
+#     def get_queryset(self):
+#         return Product.objects.select_related('category').all()
+#
+#     def get_serializer_class(self):
+#         return ProductsSerializers
+#
+#     def get_serializer_context(self):
+#         return self.request
+#
+#
+# class ProductDeletes(DestroyAPIView):
+#
+#     def get_queryset(self):
+#         return Product.objects.select_related('category').all()
+#
+#     def get_serializer_class(self):
+#         return ProductsSerializers
+#
+#     def get_serializer_context(self):
+#         return self.request
