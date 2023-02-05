94332ca404fea87a1b30235e3984248d5be5ed3d
saqib-devops <saqibahmad778866@gmail.com>
2023-02-05 03:14:17 -0500

    MODELVIEWSET API VIEWS : product and category router added

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
+
 from .views import ProductListCreateApiView, ProductRetrieveUpdateDeleteApiView, \
-    CategoryRetrieveUpdateDeleteApiView, CategoryListCreateApiView
+    CategoryRetrieveUpdateDeleteApiView, CategoryListCreateApiView, ProductViewSet, CategoryViewSet
 
 urlpatterns = [
     path("product/", ProductListCreateApiView.as_view()),
@@ -11,3 +13,10 @@ urlpatterns += [
     path('category/', CategoryListCreateApiView.as_view(), name='category-list'),
     path('category/<str:pk>/', CategoryRetrieveUpdateDeleteApiView.as_view(), name='category-detail')
 ]
+
+"""__________________ROUTERS FOR MODEL VIEWSET_____________________"""
+router = DefaultRouter()
+router.register('products', ProductViewSet)
+router.register('categories', CategoryViewSet)
+
+urlpatterns += router.urls
